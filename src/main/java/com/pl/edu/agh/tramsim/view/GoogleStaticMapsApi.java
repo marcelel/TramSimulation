0472a53e7c4a76ec17fcf8a1754cd3ce23dea249
djwojtas <djwojtas-96@o2.pl>
2017-11-27 23:13:09 +0100

    google maps inba WIP

--- src/main/java/com/pl/edu/agh/tramsim/view/GoogleStaticMapsApi.java
+++ src/main/java/com/pl/edu/agh/tramsim/view/GoogleStaticMapsApi.java
@@ -2,12 +2,19 @@ package com.pl.edu.agh.tramsim.view;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.net.URL;
 
 public class GoogleStaticMapsApi {
+
+    private static final double zoomOffset = 397.44;
+    private static final int retryTime = 100;
+    private static final int retryCount = 5;
+    private static Logger logger = LoggerFactory.getLogger( GoogleStaticMapsApi.class );
 
     public static Image requestImage(double latitude, double longitude, int width, int height, int zoom) throws IOException {
         URL imageURL = new URL("http://maps.googleapis.com/maps/api/staticmap?center=" +
@@ -16,6 +23,26 @@ public class GoogleStaticMapsApi {
                 width + "x" +
                 height + "&zoom=" +
                 zoom);
-        return SwingFXUtils.toFXImage(ImageIO.read(imageURL), null);
+
+        for(int i=0;; ++i) {
+            try {
+                return SwingFXUtils.toFXImage(ImageIO.read(imageURL), null);
+            } catch (IOException e) {
+                if(i == retryCount) {
+                    logger.debug("Error loading tile for the " + (i-1) + " time");
+                    throw e;
+                }
+                try {
+                    Thread.sleep(retryTime);
+                    logger.trace("Retrying map tile download...");
+                } catch (InterruptedException ignored) {}
+            }
+        }
+    }
+
+    public static Image requestOffsettedImage(double latitude, double longitude, int width, int height, int zoom, int tilesY, int tilesX) throws IOException {
+        latitude += (tilesY/Math.pow(2, zoom-1))*zoomOffset;
+        longitude += (tilesX/Math.pow(2, zoom-1))*zoomOffset;
+        return requestImage(latitude, longitude, width, height, zoom);
     }
 }
--- src/main/java/com/pl/edu/agh/tramsim/view/MainApplication.java
+++ src/main/java/com/pl/edu/agh/tramsim/view/MainApplication.java
@@ -3,64 +3,107 @@ package com.pl.edu.agh.tramsim.view;
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.net.URL;
 
 public class MainApplication extends Application {
+
+    private static Logger logger = LoggerFactory.getLogger( MainApplication.class );
+
+    private int currentZoom = 10;
+
+    private double deltaDragX;
+    private double deltaDragY;
+
+    private ImageView iv1 = new ImageView();
+    private ImageView iv2 = new ImageView();
+
     public static void main(String[] args) throws IOException {
+        logger.info("Starting app");
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-//"http://maps.googleapis.com/maps/api/staticmap?center=48.167432,10.533072&size=565x565&zoom=7"
+        Image img = GoogleStaticMapsApi.requestImage(52.1, 21.1, 565, 565, currentZoom);
+        Image img2 = GoogleStaticMapsApi.requestOffsettedImage(52.1, 21.1, 565, 565, currentZoom, 0, 1);
 
-        Image img = GoogleStaticMapsApi.requestImage(52.2297, 21.0122, 565, 565, 7);
-
-        System.out.println(img.getProgress());
-
-        ImageView iv1 = new ImageView();
         iv1.setImage(img);
+
+        iv2.setImage(img2);
 
         double deltax;
 
         Group root = new Group();
         Scene scene = new Scene(root);
         scene.setFill(Color.BLACK);
-        HBox box = new HBox();
+        Pane box = new Pane();
         box.getChildren().add(iv1);
+        box.getChildren().add(iv2);
         root.getChildren().add(box);
 
-        box.setLayoutX(100);
+        box.setOnScroll(event -> {
+            double deltaY = event.getDeltaY();
+            logger.trace("Scrolling detected");
+            if (deltaY < 0){
+                updateImages(false);
+            } else {
+                updateImages(true);
+            }
+        });
 
         primaryStage.setTitle("ImageView");
         primaryStage.setScene(scene);
         primaryStage.setWidth(1280);
         primaryStage.setHeight(720);
 
-        iv1.setOnMousePressed(mouseEvent -> {
-            System.out.println(scene.getX());
-            System.out.println(mouseEvent.getScreenX());
+        iv2.setX(565);
+
+        box.setOnMousePressed(mouseEvent -> {
+            logger.trace("Mouse pressed on " + mouseEvent.getScreenX());
+            deltaDragX = mouseEvent.getScreenX();
         });
 
-        iv1.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent mouseEvent) {
-                System.out.println(mouseEvent.getScreenX());
-            }
+        box.setOnMouseDragged(mouseEvent -> {
+            iv1.setX(iv1.getX() + mouseEvent.getScreenX() - deltaDragX);
+            iv2.setX(iv2.getX() + mouseEvent.getScreenX() - deltaDragX);
+            deltaDragX = mouseEvent.getScreenX();
         });
 
-        iv1.setFocusTraversable(true);
+        box.setOnMouseReleased(mouseEvent -> {
+            logger.trace("Mouse released on " + mouseEvent.getScreenX());
+        });
 
         primaryStage.show();
+    }
+
+    private void updateImages(boolean zoomIn) {
+        try {
+            if(currentZoom > 1 && currentZoom <= 12) {
+                currentZoom += zoomIn ? 1 : -1;
+
+                iv1.setImage(GoogleStaticMapsApi.requestImage(52.1, 21.1, 565, 565, currentZoom));
+                iv2.setImage(GoogleStaticMapsApi.requestOffsettedImage(52.1, 21.1, 565, 565, currentZoom, 0, 1));
+            }
+        } catch (IOException e) {
+            logger.warn("Map zoom update failed");
+        }
     }
 }
