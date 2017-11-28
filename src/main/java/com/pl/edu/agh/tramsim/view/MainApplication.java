777568da9b45b418d784096825c864bbe969b8d6
djwojtas <djwojtas-96@o2.pl>
2017-11-28 01:59:04 +0100

    google maps inba WIP2

--- src/main/java/com/pl/edu/agh/tramsim/view/MainApplication.java
+++ src/main/java/com/pl/edu/agh/tramsim/view/MainApplication.java
@@ -1,27 +1,17 @@
 package com.pl.edu.agh.tramsim.view;
 
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.imageio.ImageIO;
 import java.io.IOException;
-import java.net.URL;
 
 public class MainApplication extends Application {
 
@@ -42,68 +32,59 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Image img = GoogleStaticMapsApi.requestImage(52.1, 21.1, 565, 565, currentZoom);
-        Image img2 = GoogleStaticMapsApi.requestOffsettedImage(52.1, 21.1, 565, 565, currentZoom, 0, 1);
+        //Image img = GoogleStaticMapsApi.requestImage(52.1, 21.1, 565, 565, 8);
+        //Image img2 = GoogleStaticMapsApi.requestOffsettedImage(52.1, 21.1, 565, 565, 8, -1, 0);
 
-        iv1.setImage(img);
+        //iv1.setImage(img);
 
-        iv2.setImage(img2);
+        //iv2.setImage(img2);
 
         double deltax;
 
         Group root = new Group();
         Scene scene = new Scene(root);
         scene.setFill(Color.BLACK);
-        Pane box = new Pane();
-        box.getChildren().add(iv1);
-        box.getChildren().add(iv2);
-        root.getChildren().add(box);
+        Pane pane = GoogleStaticMapsApi.createMapPane(52, 21, 1280,720, 8).getPane();
+//        pane.getChildren().add(iv1);
+//        pane.getChildren().add(iv2);
+        iv2.setY(565);
+        root.getChildren().add(pane);
 
-        box.setOnScroll(event -> {
-            double deltaY = event.getDeltaY();
-            logger.trace("Scrolling detected");
-            if (deltaY < 0){
-                updateImages(false);
-            } else {
-                updateImages(true);
-            }
-        });
+//        box.setOnScroll(event -> {
+//            double deltaY = event.getDeltaY();
+//            logger.trace("Scrolling detected");
+//            if (deltaY < 0){
+//                updateImages(false);
+//            } else {
+//                updateImages(true);
+//            }
+//        });
 
-        primaryStage.setTitle("ImageView");
+        primaryStage.setTitle("TramSim");
         primaryStage.setScene(scene);
         primaryStage.setWidth(1280);
-        primaryStage.setHeight(720);
+        primaryStage.setHeight(1080);
 
-        iv2.setX(565);
-
-        box.setOnMousePressed(mouseEvent -> {
-            logger.trace("Mouse pressed on " + mouseEvent.getScreenX());
-            deltaDragX = mouseEvent.getScreenX();
-        });
-
-        box.setOnMouseDragged(mouseEvent -> {
-            iv1.setX(iv1.getX() + mouseEvent.getScreenX() - deltaDragX);
-            iv2.setX(iv2.getX() + mouseEvent.getScreenX() - deltaDragX);
-            deltaDragX = mouseEvent.getScreenX();
-        });
-
-        box.setOnMouseReleased(mouseEvent -> {
-            logger.trace("Mouse released on " + mouseEvent.getScreenX());
-        });
+//        iv2.setX(565);
+//
+//        box.setOnMousePressed(mouseEvent -> {
+//            logger.trace("Mouse pressed on " + mouseEvent.getScreenX());
+//            deltaDragX = mouseEvent.getScreenX();
+//        });
+//
+//        box.setOnMouseDragged(mouseEvent -> {
+//            iv1.setX(iv1.getX() + mouseEvent.getScreenX() - deltaDragX);
+//            iv2.setX(iv2.getX() + mouseEvent.getScreenX() - deltaDragX);
+//            deltaDragX = mouseEvent.getScreenX();
+//        });
+//
+//        box.setOnMouseReleased(mouseEvent -> {
+//            logger.trace("Mouse released on " + mouseEvent.getScreenX());
+//        });
 
         primaryStage.show();
-    }
 
-    private void updateImages(boolean zoomIn) {
-        try {
-            if(currentZoom > 1 && currentZoom <= 12) {
-                currentZoom += zoomIn ? 1 : -1;
-
-                iv1.setImage(GoogleStaticMapsApi.requestImage(52.1, 21.1, 565, 565, currentZoom));
-                iv2.setImage(GoogleStaticMapsApi.requestOffsettedImage(52.1, 21.1, 565, 565, currentZoom, 0, 1));
-            }
-        } catch (IOException e) {
-            logger.warn("Map zoom update failed");
-        }
+        int a = 1;
+        System.out.println(a);
     }
 }
